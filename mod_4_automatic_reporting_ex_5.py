9c3b7d86031ad8b99b1c2c2d0f867fb59cb6f92a
aaronba2 <arounaba224@gmail.com>
2026-05-18 14:03:57 +0200

    Update mod_4_automatic_reporting_ex_5.py

--- mod_4_automatic_reporting_ex_5.py
+++ mod_4_automatic_reporting_ex_5.py
@@ -2,7 +2,22 @@ import pandas as pd
 
 df = pd.read_csv("positions.csv")
 
-rapport = df.groupby("Produit")["PnL"].sum()
+def rapport():
 
-print("=== RAPPORT AUTOMATIQUE ===")
-print(rapport)
+    nombre_positions = len(df)
+
+    pnl_total = df["PnL"].sum()
+
+    positions_perdantes = df[df["PnL"] < 0]
+
+    print("=== RAPPORT AUTOMATIQUE ===")
+
+    print("\nNombre positions :", nombre_positions)
+
+    print("PnL total :", pnl_total)
+
+    print("\n=== POSITIONS PERDANTES ===")
+
+    print(positions_perdantes)
+
+rapport()
