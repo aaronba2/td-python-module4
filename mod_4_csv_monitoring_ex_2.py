5200ebffebfa8b27adc19d0d2b83919fb7cda19f
aaronba2 <arounaba224@gmail.com>
2026-05-18 13:54:45 +0200

    Update mod_4_csv_monitoring_ex_2.py

--- mod_4_csv_monitoring_ex_2.py
+++ mod_4_csv_monitoring_ex_2.py
@@ -3,4 +3,11 @@ import pandas as pd
 df = pd.read_csv("positions.csv")
 
 print(df)
-print("\nNombre total de positions :", len(df))
+
+nombre_positions = len(df)
+
+pnl_total = df["PnL"].sum()
+
+print("\nNombre total de positions :", nombre_positions)
+
+print("PnL total :", pnl_total)
